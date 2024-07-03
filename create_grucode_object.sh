0c0b68a3864a59c4671a3e6b5a513ae4fb9c2b43
username <email>
2024-07-03 17:30:21 -0400

    use sdk naming

--- create_grucode_object.sh
+++ create_grucode_object.sh
@@ -9,18 +9,18 @@
     echo .section .text >> object.s
     echo  >> object.s
     echo .balign 16 >> object.s
-    echo glabel $1\_fifoTextStart >> object.s
+    echo glabel gsp$1\_fifoTextStart >> object.s
     echo     .incbin \"build/$1/$1.code\" >> object.s
     echo .balign 16 >> object.s
-    echo glabel $1\_fifoTextEnd >> object.s
+    echo glabel gsp$1\_fifoTextEnd >> object.s
     echo  >> object.s
     echo .section .data >> object.s
     echo  >> object.s
     echo .balign 16 >> object.s >> object.s
-    echo glabel $1\_fifoDataStart >> object.s
+    echo glabel gsp$1\_fifoDataStart >> object.s
     echo     .incbin \"build/$1/$1.data\" >> object.s
     echo .balign 16 >> object.s
-    echo glabel $1\_fifoDataEnd >> object.s
+    echo glabel gsp$1\_fifoDataEnd >> object.s
     echo >> object.s
     
-    mips64-linux-gnu-as -march=vr4300 -mabi=32 -I . object.s -o build/$1/$1.fifo.o
+    mips64-linux-gnu-as -march=vr4300 -mabi=32 -I . object.s -o build/$1/gsp$1.fifo.o
