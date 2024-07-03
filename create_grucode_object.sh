d2a56aa8056178f0bdbbf3e7bb811267f6acfb7d
username <email>
2024-07-03 17:18:07 -0400

    .

---
+++ create_grucode_object.sh
@@ -0,0 +1,26 @@
+    # Create object files
+
+    echo .macro glabel label > object.s
+    echo     .global \\label >> object.s
+    echo     .balign 4 >> object.s
+    echo     \\label: >> object.s
+    echo .endm >> object.s
+    echo  >> object.s
+    echo .section .text >> object.s
+    echo  >> object.s
+    echo .balign 16 >> object.s
+    echo glabel $1\_fifoTextStart >> object.s
+    echo     .incbin \"build/$1/$1.code\" >> object.s
+    echo .balign 16 >> object.s
+    echo glabel $1\_fifoTextEnd >> object.s
+    echo  >> object.s
+    echo .section .data >> object.s
+    echo  >> object.s
+    echo .balign 16 >> object.s >> object.s
+    echo glabel $1\_fifoDataStart >> object.s
+    echo     .incbin \"build/$1/$1.data\" >> object.s
+    echo .balign 16 >> object.s
+    echo glabel $1\_fifoDataEnd >> object.s
+    echo >> object.s
+    
+    mips64-linux-gnu-as -march=vr4300 -mabi=32 -I . object.s -o build/$1/$1.fifo.o
